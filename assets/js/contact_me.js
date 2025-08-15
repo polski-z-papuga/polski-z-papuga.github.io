29111561707871c960149ebfe86b4dca495c3486
Alex <alesssandro@ukr.net>
2025-08-15 12:47:44 +0300

    Initial commit

---
+++ assets/js/contact_me.js
@@ -0,0 +1,108 @@
+// ---
+// ---
+
+// $(function() {
+//   $('#sendMessageButton').click(function(event) {
+//     event.preventDefault();
+//     var name = $("input#name").val();
+//     var email = $("input#email").val();
+//     var phone = $("input#phone").val();
+//
+//     const token = '8420999656:AAGAk0YRgT2uvs9hnAtHbsHZVRzAl7AzUtw';
+//     const chatId = '451229866';
+//     var textMessage = name + "\n" + phone + "\n" + email;
+//
+//     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(textMessage)}`;
+//
+//
+//     fetch(url)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         alert('Comment sent successfully!');
+//         document.getElementById('commentText').value = '';
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//       });
+//   });
+// });
+
+$(function() {
+  var formattedMessage = function(name, phone, email) {
+    const line = "\n  --------------  \n";
+    var msg = "Ім’я: \n" + name + line + "Номер мобільного: \n" + phone + line + "Імейл: \n" + email;
+    return encodeURIComponent(msg);
+  };
+
+  $("#contactForm input").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+
+      const token = '8420999656:AAGAk0YRgT2uvs9hnAtHbsHZVRzAl7AzUtw';
+      const chatIds = ['451229866', '687012041'];
+      var textMessage = formattedMessage(name, phone, email);
+
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+
+      chatIds.forEach(function(chatId){
+        let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${textMessage}`;
+        console.warn(url);
+
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+              .append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+          })
+          .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+          });
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
